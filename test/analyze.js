90da561ee634f4f5f7776d047ec60531a51aa374
Patrick Stein <lylepstein@gmail.com>
2013-09-13 15:27:25 -0700

    Tests for analysis

---
+++ test/analyze.js
@@ -0,0 +1,78 @@
+var acorn = require('acorn')
+  , _analyze = require('../lib/analyze')
+  ;
+
+var bodyRe = /^function\s*\(\)\s*{\s*([\s\S]*?)\s*}$/;
+var analyze = function(f) {
+  return _analyze(acorn.parse(f.toString().match(bodyRe)[1], {
+    locations: true
+  }));
+};
+
+exports.testDirectiveRequire = function(test) {
+  test.deepEqual(analyze(function() {
+    'akom require: foo bar';
+  }), {
+    provides: [],
+    requires: ['foo', 'bar']
+  });
+
+  test.done();
+};
+
+exports.testDirectiveProvide = function(test) {
+  test.deepEqual(analyze(function() {
+    'akom provide: foo bar';
+  }), {
+    provides: ['foo', 'bar'],
+    requires: []
+  });
+
+  test.done();
+};
+
+exports.testDirectiveRequireWithProvidesSelf = function(test) {
+  test.deepEqual(analyze(function() {
+    'akom require: foo';
+    'akom provide: foo bar';
+  }), {
+    provides: ['foo', 'bar'],
+    requires: []
+  });
+
+  test.done();
+};
+
+
+exports.testGlobalVariableProvides = function(test) {
+  test.deepEqual(analyze(function() {
+    var a = 1;
+  }), {
+    provides: ['a'],
+    requires: []
+  });
+
+  test.done();
+};
+
+exports.testBasicRequires = function(test) {
+  test.deepEqual(analyze(function() {
+    b + 1;
+  }), {
+    provides: [],
+    requires: ['b']
+  });
+
+  test.done();
+};
+
+exports.testBasicRequiresInIIFE = function(test) {
+  test.deepEqual(analyze(function() {
+    (function() { return b })();
+  }), {
+    provides: [],
+    requires: ['b']
+  });
+
+  test.done();
+};
